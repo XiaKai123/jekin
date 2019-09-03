db5fc0f67eb037e7e7b7ca70a55beec5bfa33d85
zhengjx <zjx123456>
2019-09-03 17:40:21 +0800

    jekin

---
+++ src/main/java/com/example/jekin/JekinApplication.java
@@ -0,0 +1,13 @@
+package com.example.jekin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JekinApplication.class, args);
+    }
+
+}
