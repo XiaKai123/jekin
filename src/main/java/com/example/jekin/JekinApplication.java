b54972097fe22575f2f2febd616ac39ad0dcdd6c
zhengjx <zjx123456>
2019-09-03 17:49:13 +0800

    jekin

--- src/main/java/com/example/jekin/JekinApplication.java
+++ src/main/java/com/example/jekin/JekinApplication.java
@@ -2,12 +2,19 @@ package com.example.jekin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JekinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JekinApplication.class, args);
+    }
+    @RequestMapping("/index")
+    public String test() {
+        return "success";
     }
 
 }
